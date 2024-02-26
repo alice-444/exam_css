01e12486412529640ff2ce3fbc2b5322bb2b635a
Arisu Ryohei <carole.2002@outlook.com>
2024-02-26 09:34:27 +0100

    structure

---
+++ app/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,jsx}",
+    "./src/components/**/*.{js,jsx}",
+    "./src/app/**/*.{js,jsx}",
+  ],
+  plugins: [],
+};
